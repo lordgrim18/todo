267953bc550c1b3f3beeed9d80541b628f2d99ae
“Jerom <darkknightjerom@gmail.com>
2023-06-10 06:54:44 +0530

    changes for vercel

--- todo/settings.py
+++ todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wd8-c*jb$sv2*e72_*ghg%=b6)*&%_pz(ly$h@7kk=(_rzvi^s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
